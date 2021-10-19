9c1f7114664ed5120ef3748b88ec47b29877ee62
kofhearts <sndpflashtv@gmail.com>
2021-10-19 09:37:32 -0400

    stats improved

--- stats.sh
+++ stats.sh
@@ -2,15 +2,15 @@
 
 target=$1
 
-echo -e "File\t\t\tFilePath";
-echo -e "Found Directories\t${target}->directory->output";
-echo -e "Found Endpoints\t${target}->Endpoints->Wayback,Paramspider";
-echo -e "Pattern Search\t${target}->Patternsearch->xss,sqli,openredirect";
-echo -e "Screenshot Subdomains\t${target}->screenshot->output";
-echo -e "Found Subdomains\t${target}->subdomains->live_subdomains";
-echo -e "Template Scan\t${target}->templatescan->Vulnerabilities";
-echo -e "Vulnerability\t${target}->Vulnerability->Vulnerability on Pattern";
-echo -e "XSS\t${target}->directory->xss_potential";
+echo -e "File\t\t\t\t\tFilePath";
+echo -e "Found Directories\t\t${target}->directory->output";
+echo -e "Found Endpoints\t\t\t${target}->Endpoints->Wayback,Paramspider";
+echo -e "Pattern Search\t\t\t${target}->Patternsearch->xss,sqli,openredirect";
+echo -e "Screenshot Subdomains\t\t${target}->screenshot->output";
+echo -e "Found Subdomains\t\t${target}->subdomains->live_subdomains";
+echo -e "Template Scan\t\t\t${target}->templatescan->Vulnerabilities";
+echo -e "Vulnerability\t\t\t${target}->Vulnerability->Vulnerability on Pattern";
+echo -e "XSS\t\t\t\t${target}->directory->xss_potential";
 
 
 
